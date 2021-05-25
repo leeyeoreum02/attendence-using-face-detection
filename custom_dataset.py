1014030ef6f3565f27b978ae95b687ca04e249f6
leeyeoreum02 <leeyeoreum01@gmail.com>
2021-05-25 18:31:29 +0900

    5/25 modify dataset error code

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple, Sequence, Callable
+from typing import Tuple, Sequence, Callable, List
 from PIL import Image
 import pandas as pd
 import numpy as np
@@ -16,23 +16,20 @@ from torch.utils.data import Dataset
 def make_labels(dir: os.PathLike = 'data') -> None:
     categories = [category for category in os.listdir(dir)
                            if os.path.isdir(os.path.join(dir, category))]
-    labels_df = pd.DataFrame(columns=['category'], index=None)
+    cat_data = [data for data in categories
+                     for _ in range(len(os.listdir(os.path.join(dir, data))))]
+    temp_df = pd.DataFrame(cat_data, columns=['category'])
 
-    row = 0
-    for category in categories:
-        for j in range(len(os.listdir(os.path.join(dir, category)))):
-            labels_df.loc[row] = category
-            row += 1
-    
-    cats = labels_df[['category']]
+    temp_cat = temp_df[['category']]
     onehot_encoder = OneHotEncoder()
-    label_encoded = onehot_encoder.fit_transform(cats)
+    label_encoded = onehot_encoder.fit_transform(temp_cat).toarray()
 
-    for row in range(labels_df.shape[0]):
-        labels_df['label1'] = label_encoded[0, 0]
-        labels_df['label2'] = label_encoded[0, 1]
 
-    labels_df.to_csv('data/labels.csv')
+    final_data = list(zip(cat_data, label_encoded[:, 0], label_encoded[:, 1]))
+    
+    final_df = pd.DataFrame(final_data, columns=['category', '0', '1'])
+
+    final_df.to_csv('data/labels.csv')
 
 
 class FaceDataset(Dataset):
@@ -85,16 +82,28 @@ class FaceDataset(Dataset):
         return len(self.image_ids)
 
 
-# if __name__ == '__main__':
-#     make_labels()
+def split_dataset(dataset_size: int, split_rate: float, num_seed: int = 42) -> Tuple[List]: 
+    indices = list(range(dataset_size))
+    split_indices = int(np.floor(split_rate * dataset_size))
 
-#     transforms = transforms.Compose([
-#         transforms.ToTensor(),
-#         transforms.Normalize(
-#             [0.485, 0.456, 0.406],
-#             [0.229, 0.224, 0.225]
-#         )
-#     ])
+    np.random.seed(num_seed)
+    np.random.shuffle(indices)
+    test_indices, train_indices = indices[:split_indices], indices[split_indices:]
+    print('\nlen(train_indices) =', len(train_indices), ', len(test_indices) =', len(test_indices), '\n')
 
-#     face = FaceDataset('data', 'data/labels.csv', transforms)
-#     print(face[978])
+    return train_indices, test_indices
+
+
+if __name__ == '__main__':
+    make_labels()
+
+    transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.485, 0.456, 0.406],
+            [0.229, 0.224, 0.225]
+        )
+    ])
+
+    face = FaceDataset('data', 'data/labels.csv', transforms)
+    print(face[978])
--- train.py
+++ train.py
@@ -4,12 +4,13 @@ import torch
 import torch.optim as optim
 from torch import nn, Tensor
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchinfo import summary
 from custom_dataset import FaceDataset
+from custom_dataset import split_dataset
 
 from torchvision import transforms
 from models import FaceDetector
-
 
 
 transform = transforms.Compose([
@@ -21,20 +22,25 @@ transform = transforms.Compose([
 ])
 
 dataset = FaceDataset('data', 'data/labels.csv', transform)
-data_loader = DataLoader(dataset, batch_size=32, num_workers=0)
+train_indices, test_indices = split_dataset(len(dataset), 0.2)
+train_sampler = SubsetRandomSampler(train_indices)
+test_sampler = SubsetRandomSampler(test_indices)
+
+train_loader = DataLoader(dataset, batch_size=32, sampler=train_sampler, num_workers=0)
+test_loader = DataLoader(dataset, batch_size=8, sampler=test_sampler, num_workers=0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = FaceDetector(num_classes=2).to(device)
 summary(model, input_size=(1, 3, 213, 160))
 
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 criterion = nn.MSELoss()
 
-num_epochs = 2
+num_epochs = 30
 
 model.train()
 for epoch in range(num_epochs):
-    for i, (images, targets) in enumerate(data_loader):
+    for i, (images, targets) in enumerate(train_loader):
         optimizer.zero_grad()
 
         images = images.to(device)
@@ -48,7 +54,25 @@ for epoch in range(num_epochs):
 
         if (i+1) % 10 == 0:
             outputs = outputs > 0.5
-            print(f'{epoch}: {loss.item():.5f}')
+            acc = (outputs == targets).float().mean()
+            print(f'epoch: {epoch}, step: {i}, loss: {loss.item():.5f}, acc: {acc.item():.5f}')
+
+with torch.no_grad():
+    model.eval()
+    for images, targets in test_loader:
+        images = images.to(device)
+        targets = targets.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, targets)
+
+        outputs = outputs > 0.5
+        acc = (outputs == targets).float().mean()
+
+print(acc.cpu().numpy())
+
+PATH = 'face_detection.pth'
+torch.save(model.state_dict(), PATH)
 
 img = Image.open('captured_2021_5_25_16_30_27.jpg').convert('RGB')
 img = transforms.ToTensor()(img)
